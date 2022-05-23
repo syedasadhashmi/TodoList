a3f59db93336518c6d1342cda01612d57d8b862c
sshashmi <sshashmi@NIS-PK132s-MacBook-Pro.local>
2022-05-23 20:56:46 +0500

    basic styling Done

---
+++ js/index.js
@@ -0,0 +1,12 @@
+
+function addToList(){
+    let searchBtn = document.getElementById("search-bar");
+    console.log(searchBtn.value);
+    searchBtn.value = "";
+}
+function editBtn(){
+    console.log("Edit btn pressed");
+}
+function deleteBtn(){
+    console.log("delete btn pressed");
+}
