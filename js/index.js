ae0c228049e48d63e3100dc712e4692f1d9b8d40
sshashmi <sshashmi@NIS-PK132s-MacBook-Pro.local>
2022-05-23 21:23:32 +0500

    last updates

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 
 function addToList(){
     let searchBtn = document.getElementById("search-bar");
+    let paraList = document.getElementById("p-1");
     console.log(searchBtn.value);
+    paraList.innerHTML = searchBtn.value;
     searchBtn.value = "";
 }
 function editBtn(){
